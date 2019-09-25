34f9f4668b73d2e1a3cd5010aeae076a6bb7010a
Timo Sand <timo.sand@iki.fi>
2019-09-25 10:06:22 +0200

    Adds storing of filter date in URL

--- src/App.js
+++ src/App.js
@@ -90,20 +90,4 @@ function Appointments() {
   return <AppointmentList />;
 }
 
-function Header() {
-  return (
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/about">About</Link>
-      </li>
-      <li>
-        <Link to="/topics">Topics</Link>
-      </li>
-    </ul>
-  );
-}
-
 export default App;
--- src/components/AppointmentList/AppointmentList.js
+++ src/components/AppointmentList/AppointmentList.js
@@ -1,17 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
+import { __RouterContext as RouterContext } from 'react-router';
+import queryString from 'query-string';
+
 import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
 import Typography from '@material-ui/core/Typography';
-import moment from 'moment';
-import MomentUtils from '@date-io/moment';
 import {
   KeyboardDatePicker,
   MuiPickersUtilsProvider,
 } from '@material-ui/pickers';
+
+import moment from 'moment';
+import MomentUtils from '@date-io/moment';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -34,13 +38,27 @@ function formatDateTime(startTime) {
 }
 
 function AppointmentList() {
+  const router = useContext(RouterContext);
   const [appointments, setAppointments] = useState([]);
   const [loading, setLoading] = useState(true);
   const classes = useStyles();
-  const [selectedDate, setSelectedDate] = useState(null);
+  const searchParams = queryString.parse(router.location.search);
+  const initialDate = searchParams.selectedDate
+    ? searchParams.selectedDate
+    : null;
+  console.log(router);
+  console.log(router.location);
+  console.log(router.location.search);
+  console.log(initialDate);
+
+  const [selectedDate, setSelectedDate] = useState(initialDate);
 
   const handleDateChange = date => {
     setSelectedDate(date);
+    router.history.push({
+      pathname: '/appointments',
+      search: `?selectedDate=${date.toISOString()}`,
+    });
   };
 
   useEffect(() => {
